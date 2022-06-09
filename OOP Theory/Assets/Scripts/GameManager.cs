03de260bb844f36c7754b180489b5236c95968a4
Kaan <haivron@gmail.com>
2022-06-09 22:44:27 +0300

    gamemanager + title screen button

---
+++ OOP Theory/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; } // ENCAPSULATION
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeScene(string sceneName) // ABSTRACTION: using only one function for changing scenes
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
